730427d5887b552a0672c43dc08433b2f09294d6
„neptuno“ <neptuno@artify.zone>
2024-02-18 16:34:21 +0100

    initial commit

---
+++ Context/WorkSphereContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Context;
+
+public class WorkSphereContext : DbContext
+{
+    public DbSet<UserModel> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite(@"Data Source=Database/WorkSphere.db");
+}
