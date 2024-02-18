f33212496729fab1822ca2a453e4310c84e2485f
Neptuno <kontaktalientrix@gmail.com>
2024-02-18 19:43:46 +0100

    added sqlite database

--- Context/WorkSphereContext.cs
+++ Context/WorkSphereContext.cs
@@ -8,5 +8,8 @@ public class WorkSphereContext : DbContext
     public DbSet<UserModel> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlite(@"Data Source=Database/WorkSphere.db");
+    {
+        optionsBuilder.UseSqlite("Data Source=/home/neptuno/Programming/c#/pva/zaverecna-prace/Database/WorkSphere.db");
+
+    }
 }
