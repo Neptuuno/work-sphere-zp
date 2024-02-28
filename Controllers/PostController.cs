7a5288ae25bb4bc5d0adb220c5fe8a621f21bf78
Neptuno <kontaktalientrix@gmail.com>
2024-02-28 18:17:49 +0100

    added post controller

---
+++ Controllers/PostController.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Context;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly WorkSphereContext _context;
+
+        public PostController(WorkSphereContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Post
+        public async Task<IActionResult> Index()
+        {
+              return _context.Posts != null ? 
+                          View(await _context.Posts.ToListAsync()) :
+                          Problem("Entity set 'WorkSphereContext.Posts'  is null.");
+        }
+
+        // GET: Post/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Posts == null)
+            {
+                return NotFound();
+            }
+
+            var postModel = await _context.Posts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (postModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(postModel);
+        }
+
+        // GET: Post/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Post/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PostType,Title,Description,PostedOn,Category,ApplicationUserId")] PostModel postModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(postModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(postModel);
+        }
+
+        // GET: Post/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Posts == null)
+            {
+                return NotFound();
+            }
+
+            var postModel = await _context.Posts.FindAsync(id);
+            if (postModel == null)
+            {
+                return NotFound();
+            }
+            return View(postModel);
+        }
+
+        // POST: Post/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PostType,Title,Description,PostedOn,Category,ApplicationUserId")] PostModel postModel)
+        {
+            if (id != postModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(postModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostModelExists(postModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(postModel);
+        }
+
+        // GET: Post/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Posts == null)
+            {
+                return NotFound();
+            }
+
+            var postModel = await _context.Posts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (postModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(postModel);
+        }
+
+        // POST: Post/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Posts == null)
+            {
+                return Problem("Entity set 'WorkSphereContext.Posts'  is null.");
+            }
+            var postModel = await _context.Posts.FindAsync(id);
+            if (postModel != null)
+            {
+                _context.Posts.Remove(postModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostModelExists(int id)
+        {
+          return (_context.Posts?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
